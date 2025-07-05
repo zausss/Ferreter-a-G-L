ee3a9024b24bd701c60070d9978077c5926cdd59
zausss <130512962+zausss@users.noreply.github.com>
2025-07-05 14:55:11 -0500

    se modifica el archivo index.html se le cambio el nombre por menu.html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define una ruta para tu página principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'menu.html'));
 });
 
 const PORT = process.env.PORT || 3000;
