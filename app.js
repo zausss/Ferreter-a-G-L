ebb96a6e04190a8012411e88e2d81c73b3106e51
zausss <130512962+zausss@users.noreply.github.com>
2025-07-04 21:36:39 -0500

    se agrego los estilos a la pagina index.html (menú) y se agrega el servidor de node express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define una ruta para tu página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
