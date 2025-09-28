0545a81f9e4980f367611b038de3dcd09f2e8829
zausss <daniel1997256@gmail.com>
2025-09-28 16:46:57 -0500

    se estendio la seccion de los usurios a 24 horas antes estaba en 60 minitos

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,7 +102,7 @@ class AuthController {
             res.cookie('token', token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                maxAge: 60 * 60 * 1000, // 60 minutos
+                maxAge: 24 * 60 * 60 * 1000, // 24 horas
                 sameSite: 'strict'
             });
 
